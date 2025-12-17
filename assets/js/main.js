91dc3bc8fd38cfe31139aaa43f9b43eb088bd4b9
Tobías Coria <tobias_coria@hotmail.com>
2025-12-17 02:33:25 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,4 @@
-console.log("CV loaded successfully");
 
-const toggleButton = document.getElementById("themeToggle");
-const body = document.body;
-
-// estado inicial desde localStorage
-const isDarkSaved = localStorage.getItem("theme") === "dark";
-
-if (isDarkSaved) {
-  body.classList.add("dark");
-  toggleButton.textContent = "☀️";
-} else {
-  toggleButton.textContent = "🌙";
-}
-
-// click
-toggleButton.addEventListener("click", () => {
-  const isDark = body.classList.toggle("dark");
-
-  toggleButton.textContent = isDark ? "☀️Light Mode" : "🌙Dark Mode";
-  localStorage.setItem("theme", isDark ? "dark" : "light");
-});
 
 const form = document.getElementById('contact-form');
 const statusText = document.getElementById('form-status');
